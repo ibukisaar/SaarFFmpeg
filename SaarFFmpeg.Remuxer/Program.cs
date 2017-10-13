b1abb1f39693824c5a835613f074369d15872440
Saar Ibuki <rockman.x8@qq.com>
2017-10-13 21:38:22 +0800

    修改Remuxer示例

--- SaarFFmpeg.Remuxer/Program.cs
+++ SaarFFmpeg.Remuxer/Program.cs
@@ -8,11 +8,14 @@ using Saar.FFmpeg.CSharp;
 namespace SaarFFmpeg.Remuxer {
 	class Program {
 		static void Main(string[] args) {
-			using (var reader = new MediaReader(@"Z:\game.dat_000004.wmv"))
-			using (var writer = new MediaWriter(@"Z:\test.mkv", reader).Initialize()) {
-				Packet packet = new Packet();
-				while (reader.ReadPacket(packet)) {
-					writer.Write(packet);
+			using (var reader = new MediaReader(@"Z:\game.dat_000004.wmv")) {
+				var decoder = reader.Decoders.OfType<VideoDecoder>().First();
+				using (var writer = new MediaRemuxer(@"Z:\test.mkv", decoder)) {
+					Packet packet = new Packet();
+					while (reader.ReadPacket(packet)) {
+						packet.StreamIndex = decoder.StreamIndex;
+						writer.Write(packet);
+					}
 				}
 			}
 		}
