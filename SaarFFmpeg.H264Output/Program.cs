9262590b49c463fa50ddd363abf97ae58e81f0e8
Saar Ibuki <rockman.x8@qq.com>
2017-10-13 16:45:34 +0800

    ...

--- SaarFFmpeg.H264Output/Program.cs
+++ SaarFFmpeg.H264Output/Program.cs
@@ -11,7 +11,7 @@ namespace SaarFFmpeg.H264Output {
 			var reader = new MediaReader(@"Z:\N-tone-MISOGI-fft.mp4");
 			var decoder = reader.Decoders.OfType<VideoDecoder>().First();
 			var packet = new Packet();
-			using (var writer = new MediaRemuxer(@"Z:\output.mp4", reader.Decoders[0], reader.Decoders[1])) {
+			using (var writer = new MediaRemuxer(@"Z:\output.h264", decoder)) {
 				while (reader.ReadPacket(packet)) {
 					writer.Write(packet);
 				}
